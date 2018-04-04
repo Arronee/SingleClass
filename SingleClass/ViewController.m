503668326250d05c02a1776950cea52d4e6e2ffa
Roc <408937078@qq.com>
2018-04-04 15:56:39 +0800

    SingleClass

---
+++ SingleClass/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SingleClass
+//
+//  Created by roc on 2018/4/4.
+//  Copyright © 2018年 roc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ SingleClass/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SingleClass
+//
+//  Created by roc on 2018/4/4.
+//  Copyright © 2018年 roc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
